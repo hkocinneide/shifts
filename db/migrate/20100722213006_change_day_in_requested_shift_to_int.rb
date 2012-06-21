2132cc02f1fbbe6efed69668858294bf2163c4b6
caseywatts <casey.s.watts@gmail.com>
2012-06-21 14:57:43 -0400

    one migration didnt work with pg

--- db/migrate/20100722213006_change_day_in_requested_shift_to_int.rb
+++ db/migrate/20100722213006_change_day_in_requested_shift_to_int.rb
@@ -1,6 +1,9 @@
 class ChangeDayInRequestedShiftToInt < ActiveRecord::Migration
   def self.up
-		change_column :requested_shifts, :day, :int
+		#change_column :requested_shifts, :day, :int
+    #pg can't do this conversion :/
+    remove_column :requested_shifts, :day
+    add_column :requested_shifts, :day, :int
   end
 
   def self.down
